be7d7024f7dcad75a241312eeb8ba9927d8ce562
razvancsk <37901410-razvancsk@users.noreply.replit.com>
2025-08-02 17:57:00 +0000

    Restored to 'c19b3fe69a05f6c105ec26e66758d26e0d5551d8'
    Replit-Restored-To: c19b3fe69a05f6c105ec26e66758d26e0d5551d8

--- client/src/pages/claim-sol.tsx
+++ client/src/pages/claim-sol.tsx
@@ -1335,7 +1335,7 @@ export default function SolRefund() {
               <img 
                 src={logoImage}
                 alt="Get Your Sol"
-                className="h-24 w-auto"
+                className="h-32 w-auto"
               />
             </div>
             
